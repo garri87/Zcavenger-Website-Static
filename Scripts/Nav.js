84a0642eda332970792d84727d4bfdbd2d3dc6ec
garri87 <pfloresphoto@hotmail.com>
2023-11-07 14:33:08 -0300

    Initial Commit

---
+++ Scripts/Nav.js
@@ -0,0 +1,11 @@
+document.getElementById("nav").innerHTML = `
+    <a href="index.html"> <img src="Img/TitleTransparent.png" id="logo"></a>
+
+    <nav>
+        <a  class="NavigationButton" onclick="landing()">Home</a>
+
+        <a  class="NavigationButton" onclick="development()" >Development</a>
+
+        <a class="DownloadButton"
+            href="https://garri87.itch.io/zcavenger/download/eyJpZCI6MTI5NDQ1MiwiZXhwaXJlcyI6MTY2MjEyODcxMH0%3d.V1gBg9hIQMlWcqx3APjQ8x%2f4RjM%3d">DOWNLOAD</a>
+    </nav>`;
